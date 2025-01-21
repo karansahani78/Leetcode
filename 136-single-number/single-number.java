456faadc6785e412d8175e23ae60bddc979c429e
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-01-22 00:25:38 +0530

    Time: 10 ms (23.78%) | Memory: 45 MB (94.32%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,20 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // brute force approach
-        for (int i = 0; i < nums.length; i++) {
-            boolean found = false;
-            for (int j = 0; j < nums.length; j++) {
-                // Compare nums[i] with other elements in the array
-                if (i != j && nums[i] == nums[j]) {
-                    found = true;
-                    break;
-                }
-            }
-            // If the element is not found in the rest of the array, return it
-            if (!found) {
-                return nums[i];
+        // optimal approach
+        HashSet<Integer>set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(!set.add(nums[i])){ // adding element to set and if already set has that element then it failed to add and remove that element
+                set.remove(nums[i]);
             }
         }
-        return -1;  // In case no result is found, though it shouldn't happen according to problem constraints
+        return set.iterator().next();
+        
     }
 }
