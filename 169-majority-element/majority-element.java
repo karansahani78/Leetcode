03b3d14ba1b3e55b3747338e846cd0d4be050f32
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-01-21 23:26:31 +0530

    Time: 2 ms (75.96%) | Memory: 53.1 MB (57.35%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,8 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // bruteForce approach
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int count = 0;  // To track "votes" for the current candidate
+        int max = 0;    // To store the current candidate
         
+        // First pass to find the majority candidate
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                max = nums[i];  // If count is 0, reset the candidate to the current number
+            }
+            if (nums[i] == max) {
+                count++;  // Increment count if current number matches candidate
+            } else {
+                count--;  // Decrement count if it doesn't match
+            }
+        }
+        
+        return max;  // Return the final candidate, which is guaranteed to be the majority element
     }
 }
