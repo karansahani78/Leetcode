9e88c36b644325cb3591bf6848a6972dc4b18a9f
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-01-23 19:03:19 +0530

    Time: 5 ms (44.07%) | Memory: 45.7 MB (39.39%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,27 +1,33 @@
+import java.util.HashSet;
+
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n = grid.length;
-        int rN = -1;
-        int totalNumber = n*n;
+        int mN = -1; // Missing Number
+        int rN = -1; // Repeated Number
+        int n = grid.length; // Size of the grid
+        int totalSize = n * n; // Total numbers expected
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+
+        // Loop through the grid to find the repeated number
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 int num = grid[i][j];
-                if(set.contains(num)){
-                    rN = num;
+                if (set.contains(num)) {
+                    rN = num; // Found the repeated number
                 } else {
-                    set.add(num);
+                    set.add(num); // Add the number to the set
                 }
             }
         }
-        int mN = -1;
-        for(int i=1; i<=totalNumber; i++){
-            if(!set.contains(i)){
-                mN =i;
-                break;
+
+        // Loop to find the missing number
+        for (int i = 1; i <= totalSize; i++) {
+            if (!set.contains(i)) {
+                mN = i; // Found the missing number
+                break; // Exit the loop after finding the first missing number
             }
         }
-        return new int[]{rN,mN};
-        
+
+        return new int[]{rN, mN}; // Return the repeated and missing numbers
     }
 }
