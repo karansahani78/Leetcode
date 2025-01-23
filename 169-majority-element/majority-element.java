15933facc160b1e84b2eee7f5c93c0e55ee78853
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-01-23 17:42:57 +0530

    Time: 1423 ms (5.02%) | Memory: 53.1 MB (57.94%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,8 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // bruteForce approach
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-        
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0; // Reset count for each candidate
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            // Check if the count of the current candidate is greater than n/2
+            if (count > nums.length / 2) {
+                return nums[i];
+            }
+        }
+        return -1; // Return -1 if no majority element is found
     }
 }
