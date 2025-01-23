8e6b81d194d0295740c2efe4577871c2ffb697b5
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-01-23 17:09:23 +0530

    Time: 4 ms (51.59%) | Memory: 53.8 MB (8.36%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count =0;
-        int maj =0;
-        for(int i=0; i<nums.length; i++){
-            if(count==0){
-                maj = nums[i];
-            } if(nums[i]==maj){ // if duplicate found than increase otherwise decrease count
-                count++;
-            } else{
-                count--;
-            }
-        }
-        return maj;
+        // bruteForce approach
+        Arrays.sort(nums);
+        return nums[nums.length/2];
         
     }
 }
