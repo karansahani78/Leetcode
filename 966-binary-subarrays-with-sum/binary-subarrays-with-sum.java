ae96877ad81c0e54ad83830ace48c1b3c14a0c02
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-03-06 23:42:51 +0530

    Time: 23 ms (25.99%) | Memory: 49.3 MB (24.40%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,19 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
-    }
+        // doing with prefix sum
+        int ps =0;
+        int result =0;
+        Map<Integer,Integer>map= new HashMap<>();
+        
+        map.put(0,1);
+        for(int i=0; i<nums.length; i++){
+            ps+=nums[i];
+            
+            result+=map.getOrDefault(ps-goal,0);
+            map.put(ps,map.getOrDefault(ps,0)+1);
 
-    private int atMost(int[] nums, int goal) {
-        if (goal < 0) return 0;  // Edge case: negative goal is impossible
-        int start = 0, sum = 0, count = 0;
-        for (int end = 0; end < nums.length; end++) {
-            sum += nums[end];
-            while (sum > goal) {
-                sum -= nums[start];
-                start++;
-            }
-            count += end - start + 1;  // Count subarrays with sum at most 'goal'
         }
-        return count;
+        return result;
+        
     }
 }
