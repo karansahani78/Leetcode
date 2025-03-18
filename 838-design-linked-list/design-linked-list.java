dc8fe5d68955da259763dea26c766877842d3970
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-03-19 03:48:07 +0530

    Time: 6 ms (98.96%) | Memory: 46 MB (19.62%) - LeetSync

--- 838-design-linked-list/design-linked-list.java
+++ 838-design-linked-list/design-linked-list.java
@@ -1,8 +1,18 @@
 class MyLinkedList {
+    
+    private class Node {  // Move Node class inside MyLinkedList
+        int val;
+        Node next;
 
-    Node head;
-    Node tail;
-    int size;
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
 
     public MyLinkedList() {
         head = null;
@@ -11,100 +21,54 @@ class MyLinkedList {
     }
     
     public int get(int index) {
-        if (index < 0 || index >= size) {
-            return -1; // Invalid index
-        }
+        if (index < 0 || index >= size) return -1;
         Node temp = head;
-        for (int i = 0; i < index; i++) {
-            temp = temp.next;
-        }
+        for (int i = 0; i < index; i++) temp = temp.next;
         return temp.val;
     }
     
     public void addAtHead(int val) {
         Node newNode = new Node(val);
-        if (head == null) {
-            head = tail = newNode;
-        } else { 
-            newNode.next = head;
-            head = newNode;
-        }
+        if (head == null) tail = newNode;
+        newNode.next = head;
+        head = newNode;
         size++;
     }
     
     public void addAtTail(int val) {
         Node newNode = new Node(val);
-        if (tail == null) {
-            head = tail = newNode;
-        } else { 
-            tail.next = newNode;
-            tail = newNode;
-        }
+        if (tail == null) head = newNode;
+        else tail.next = newNode;
+        tail = newNode;
         size++;
     }
     
     public void addAtIndex(int index, int val) {
-        if (index < 0 || index > size) {  // Fixed condition
-            System.out.println("Invalid index");
-            return;
-        }
-        Node newNode = new Node(val);
+        if (index < 0 || index > size) return;
         if (index == 0) {
             addAtHead(val);
             return;
         }
         Node temp = head;
-        for (int i = 0; i < index - 1; i++) { // Fixed loop condition
-            temp = temp.next;
-        }
+        for (int i = 0; i < index - 1; i++) temp = temp.next;
+        Node newNode = new Node(val);
         newNode.next = temp.next;
         temp.next = newNode;
-        if (newNode.next == null) { // If added at the end, update tail
-            tail = newNode;
-        }
+        if (newNode.next == null) tail = newNode;
         size++;
     }
     
     public void deleteAtIndex(int index) {
-        if (index < 0 || index >= size) {  // Fixed condition
-            System.out.println("Invalid index");
-            return;
-        }
+        if (index < 0 || index >= size) return;
         if (index == 0) {
             head = head.next;
-            if (head == null) { // If list becomes empty, update tail
-                tail = null;
-            }
+            if (head == null) tail = null;
         } else {
             Node temp = head;
-            for (int i = 0; i < index - 1; i++) { // Fixed loop condition
-                temp = temp.next;
-            }
+            for (int i = 0; i < index - 1; i++) temp = temp.next;
             temp.next = temp.next.next;
-            if (temp.next == null) { // If last element is deleted, update tail
-                tail = temp;
-            }
+            if (temp.next == null) tail = temp;
         }
         size--;
     }
 }
-
-class Node {
-    int val;
-    Node next;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
