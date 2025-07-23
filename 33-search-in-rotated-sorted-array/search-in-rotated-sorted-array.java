62039633ddb05a2a7085df397e2b334c03d8cd74
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-07-24 01:49:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (92.96%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,14 +1,48 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==0){
-            return -1;
-        }
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
-                return i;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        // Finding smallest value (pivot)
+        while (start < end) { // FIXED CONDITION
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
         }
+
+        int pivot = start; // Also fixed earlier
+
+        // Search in left part
+        start = 0;
+        end = pivot - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] < target){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        // Search in right part
+        start = pivot;
+        end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] < target){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
         return -1;
-        
     }
 }
