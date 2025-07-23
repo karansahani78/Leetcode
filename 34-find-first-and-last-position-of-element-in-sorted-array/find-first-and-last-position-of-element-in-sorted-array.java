7245dc6594bd234efd0f29c3807b419101377b5a
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-07-24 01:07:32 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (47.43%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int left=0;
+        int right =nums.length-1;
         int [] ans = {-1,-1};
-        int start =0;
-        int end =nums.length-1;
-        while(start<=end){
-            int mid =start+(end-start)/2;
+        while(left<=right){
+            int mid = left+(right-left)/2;
             if(nums[mid]<target){
-                start = mid+1;
-            } else if(nums[mid]>target){
-                end = mid-1;
-            }else{
-                ans[0]=mid;
-                // left check on left if second element available or what
-                end = mid-1;
-
+                left = mid+1;
+            }else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid;
+                right =mid;
+                // Found target, now expand to both sides
+                while(left>0 && nums[left-1]==target){
+                    left--;
+                }
+                while(right<nums.length-1 && nums[right+1]==target){
+                    right++;
+                }
+                ans[0]=left;
+                ans[1]=right;
+                break;
+                
             }
         }
-        start = 0;
-        end = nums.length-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(nums[mid]<target){
-                start =mid+1;
-            } else if(nums[mid]>target){
-                end = mid-1;
-            }else{
-                ans[1]=mid;
-                // chalo ek bar right ko check karlete hai 
-                start = mid+1;
-            }
-        }
-return ans;
-
+        return ans;
+        
     }
 }
