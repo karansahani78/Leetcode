174667be2bc50827cf9e55e44674607d9a30ae9d
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-08-30 19:05:25 +0545

    Time: 15 ms (42.87%) | Memory: 52.6 MB (34.62%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,19 +1,21 @@
-class MyHashSet<T> {
-    private int SIZE=7;
-    private LinkedList<T>[]bucket;
-   
+class MyHashSet {
+    private int SIZE=769;
+    private LinkedList<Integer>[] bucket;
+
 
     public MyHashSet() {
         bucket = new LinkedList[SIZE];
         for(int i=0; i<SIZE; i++){
             bucket[i]= new LinkedList<>();
-        }    
+        }
+        
     }
-    private int hash(T key){
-        return Math.abs(key.hashCode()) %SIZE;
+    // hash function to calculate actual bucket to put element as per their size 
+    private int hash(int key){
+        return key % SIZE;
     }
     
-    public void add(T key) {
+    public void add(int key) {
         int index = hash(key);
         if(!bucket[index].contains(key)){
             bucket[index].add(key);
@@ -21,17 +23,15 @@ class MyHashSet<T> {
         
     }
     
-    public void remove(T key) {
+    public void remove(int key) {
         int index = hash(key);
-        bucket[index].remove(key);
+            bucket[index].remove((Integer)key);
+        }
         
-    }
-    
-    public boolean contains(T key) {
+    public boolean contains(int key) {
+        
         int index = hash(key);
         return bucket[index].contains(key);
-
-        
     }
 }
 
