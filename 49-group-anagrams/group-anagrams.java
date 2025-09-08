41f7b5c3d9e317ea048a60f0178c644e07f3241e
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-09-09 01:49:58 +0530

    Time: 6 ms (98.77%) | Memory: 47.8 MB (83.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,37 +1,24 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
+        
+       Map<String, List<String>> map = new HashMap<>();
+        for(int i=0; i<strs.length; i++){
+            String currentS = strs[i];
+            char [] arrS = currentS.toCharArray();
+            Arrays.sort(arrS);
 
-        // Step 1: Process each word
-        for (int i = 0; i < strs.length; i++) {
-            String word = strs[i];  // take the word
-
-            // convert word to char array so we can sort it
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-
-            // make a new string from sorted chars (this will be the key)
-            String key = new String(chars);
-
-            // if key is not in map, put new empty list
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<String>());
-            }
-
-            // add the word into the correct list
-            map.get(key).add(word);
+        // sorted String
+        String sortedStr = new String(arrS);
+        if(!map.containsKey(sortedStr)){
+            map.put(sortedStr, new ArrayList<>());
+            
         }
-
-        // Step 2: Convert map values into result list
-        List<List<String>> result = new ArrayList<>();
-        List<String> keys = new ArrayList<>(map.keySet()); // all keys in a list
-
-        for (int i = 0; i < keys.size(); i++) {
-            result.add(map.get(keys.get(i)));
-        }
-
-        return result;
+         // Add original string to the list corresponding to the sorted key
+         map.get(sortedStr).add(currentS);
+        
+    
     }
+      // Collect all grouped anagrams into the result list
+      return new ArrayList<>(map.values());
+}
 }
