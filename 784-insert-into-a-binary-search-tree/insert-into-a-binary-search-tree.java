b65e44d396bcc4d8af482a45e39e5c60869778a0
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-09-14 20:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (93.89%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -1,31 +1,19 @@
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
+        // Base case: if current node is null, create and return a new node with val
         if (root == null) {
             return new TreeNode(val);
         }
 
-        TreeNode temp = root;
-
-        while (true) {
-            if (val < temp.val) {
-                // Go to left subtree
-                if (temp.left == null) {
-                    temp.left = new TreeNode(val);
-                    break;
-                } else {
-                    temp = temp.left;
-                }
-            } else {
-                // Go to right subtree
-                if (temp.right == null) {
-                    temp.right = new TreeNode(val);
-                    break;
-                } else {
-                    temp = temp.right;
-                }
-            }
+        // If the value to insert is less than the current node's value, go to left subtree
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            // Otherwise, go to right subtree
+            root.right = insertIntoBST(root.right, val);
         }
-        
+
+        // Return the root node after insertion
         return root;
     }
 }
