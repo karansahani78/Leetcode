cbe5f6dc273c8b08af32cf37a98aa8eaa4bc2030
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2025-09-22 06:56:45 +0530

    Time: 14 ms (57.08%) | Memory: 49.6 MB (6.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,24 +1,31 @@
 class Solution {
+    class Number implements Comparable<Number>{
+        int element;
+        int frequency;
+        public Number(int element, int frequency){
+            this.element = element;
+            this.frequency=frequency;
+        }
+        // override the method that comparre
+        public int compareTo(Number that){
+            return that.frequency - this.frequency; // sorted in descending order or decreasing order
+        }
+
+    }
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> freq = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+        PriorityQueue<Number> pq = new PriorityQueue<>();
+        for(int num:nums){
+            freq.put(num,freq.getOrDefault(num,0)+1);
         }
-        // now create a array for the result having k size
-        int [] result = new int[k];
-        // now lets check which element is more freq count and we will do this k times
+        // now put get the element fromt the map with freq and put(offer) them into PriorityQueue
+        for(int key:freq.keySet()){
+            pq.offer(new Number(key,freq.get(key)));
+        }
+        // now get the most freqency element from the priorityQueue and add them inside the result array
+        int [] result = new int [k];
         for(int i=0; i<k; i++){
-            int maxFreq =-1;
-            int maxFnumber= -1; // this is the number that has maximum freq
-              // find the element with the highest frequency
-              for(int number :freq.keySet()){
-                if(freq.get(number)>maxFreq){
-                    maxFreq = freq.get(number);
-                    maxFnumber =number;
-                }
-              }
-              result[i]=maxFnumber;
-              freq.remove(maxFnumber);
+            result[i] = pq.poll().element;
         }
         return result;
         
